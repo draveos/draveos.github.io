2329b4136e0ac7c913321182fc01201ed4adefd7
draveos <draveos20@gmail.com>
2025-06-02 05:45:32 +0900

    build for page

--- vite.config.tsx
+++ vite.config.tsx
@@ -4,4 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [tailwindcss(), react()],
+  base: "/",            // 사용자 페이지용 GitHub Pages면 보통 "/"로 둡니다.
+  build: {
+    outDir: "docs",     // dist 대신 docs 폴더에 빌드 결과물이 생성되도록
+    emptyOutDir: true,  // 빌드 전에 docs/ 내부를 비워줌
+  }
 })
